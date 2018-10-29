09acb8f5e6398365d2fb584683a4eb2b27b6f32a
Muhammad Ardiansyah <dianbugas@gmail.com>
2018-10-29 17:11:44 +0700

    bilangan genap C++

---
+++ genap1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int n;
+cout<<"masukan bilangan 0 :";
+cin>>n;
+
+for (n=0; n<=20; n++)
+if (n%2==0){
+cout << n << " " ;
+} else {
+//tidak ada aksi
+}
+}
+
