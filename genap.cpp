e4657638dba8103cdef4a11d057c4c8fc51a0d0b
Muhammad Ardiansyah <dianbugas@gmail.com>
2018-11-04 21:04:35 +0700

    penambahan

--- genap.cpp
+++ genap.cpp
@@ -3,13 +3,13 @@ using namespace std;
 int main()
 {
     int angka;
-    cout <<"Masukkan angka :";
+    cout <<"Masukkan angka 0:";
     cin>>angka;
         for (angka=0; angka<=20; angka++)
         if(angka%2==0)
         {
             cout<< angka <<" ";
         }else{
-        //tidak ada aksi
+        //tidak ada aksi di dalamnya
         }
 }
