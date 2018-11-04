65b1a3004f047608b2277391a35e3bbdca523f50
Muhammad Ardiansyah <dianbugas@gmail.com>
2018-11-04 21:07:26 +0700

    copy

---
+++ genap (copy).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int angka;
+    cout <<"Masukkan angka 0:";
+    cin>>angka;
+        for (angka=0; angka<=20; angka++)
+        if(angka%2==0)
+        {
+            cout<< angka <<" ";
+        }else{
+        //tidak ada aksi di dalamnya
+        }
+}
